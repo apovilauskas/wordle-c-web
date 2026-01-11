f11f87204f110ad1a5e06dd5286e536d86a64f46
Augustas Povilauskas <augustaspovil@gmail.com>
2026-01-11 09:21:51 +0200

    Update http_handler.h

--- backend/http_handler.h
+++ backend/http_handler.h
@@ -3,5 +3,7 @@
 
 void handle_http_request(int socket);
 void send_response(int socket, int status_code, const char* content_type, const char* body);
+void handle_new_game(int socket);
+void handle_guess(int socket, char* request);
 
 #endif
